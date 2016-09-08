5100d1cba7121432b0b70391a9f461058b470fd8
victorlian <vlia679@aucklanduni.ac.nz>
2016-09-08 14:09:14 +1200

    Speech class implemented

--- spellingAid/Speech.java
+++ spellingAid/Speech.java
@@ -1,5 +1,7 @@
 package spellingAid;
 
+import java.io.IOException;
+
 /**
  * This is the class representing the speech synthesis.
  * Responsibilities:
@@ -17,7 +19,7 @@ public class Speech {
 	 * @param word
 	 */
 	public void sayWord(Word word){
-		
+		festivalSayIt(word.toString());
 	}
 	
 	/**
@@ -25,6 +27,37 @@ public class Speech {
 	 * @param toSay
 	 */
 	public void saySentence(String toSay){
+		festivalSayIt(toSay);
+	}
+	
+	
+	/**
+	 * This method will set the voice of festival, run this method
+	 * prior to saying anything.
+	 * @param voice
+	 */
+	public void setVoice (String voice){
 		
+	}
+	
+	
+	/**
+	 * This is a private helper method that will
+	 * allow bash-festival to say the input.
+	 * @param toSay
+	 * @return
+	 */
+	private void festivalSayIt (String toSay){
+		String cmd = "echo \"" + toSay + "\" | festival --tts ";
+		ProcessBuilder builder = new ProcessBuilder ("/bin/bash", "-c", cmd);
+		try {
+			Process process = builder.start();
+			process.waitFor();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return;
 	}
 }
